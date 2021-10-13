ff83c4c35137443a0c42a1b094ba46337f8c4f9f
MarkHF <markherbertfrancisco@gmail.com>
2021-10-12 18:37:39 -0700

    database connection

---
+++ config/config.php
@@ -0,0 +1,6 @@
+<?php
+
+define("DBUSER", "root");
+define("DBPASS", "group5");
+define("DBNAME", "jobsourcing");
+define("DBHOST", "LocalHost");
---
+++ lib/database.php
@@ -0,0 +1,62 @@
+<?php
+
+class Database{
+    private $host = DBHOST;
+    private $user = DBUSER;
+    private $pass = DBPASS
+    private $dbname = DBNAME
+
+    private $dbh;
+    private $error;
+    private $stmt;
+
+    public function _construct(){
+        $dsn = 'mysql:host='. $this->host .',dbname='. $this->dbname;
+        $option =array(
+            PDO::ATTR_PERSISTENT => true,
+            PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION
+        );
+        try {
+            $this->dbh = new PDO($dsn, $this->user, $this->pass, $option);
+        } catch (PDOException $e) {
+            $this->error = $e->getMessage();
+        }
+    }
+
+    public function query($query){
+        $this->stmt = $this->dbh->prepare($query);
+    }
+
+    public function bind($param, $value, $type = null){
+        if(is_null($type)){
+            switch(true){
+                case is_int ($value) :
+                    $type = PDO::PARAM_INIT;
+                    break;
+                case is_bool ($value) :
+                    $type = PDO::PARAM_BOOL;
+                    break;
+                case is_null ($value) :
+                    $type = PDO::PARAM_NULL;
+                    break;
+                default :
+                    $type = PDO::PARAM_STR;
+            }
+        }
+        $this->stmt->bindValue($param, $value, $type);
+    }
+
+    public function execute(){
+        return $this->stmt->execute();
+    }
+
+    public function resultSet(){
+        $this->execute();
+        return $this->stmt->fetchAll(PDO::FETCH_OBJ);
+    }
+
+    public function single(){
+        $this->execute();
+        return $this->stmt->fetch(PDO::FETCH_OBJ);
+    }
+}
