860fdf5884b3782703a80a9164d1fa66f1d7ab1f
MarkHF <markherbertfrancisco@gmail.com>
2021-10-23 19:59:03 -0700

    bago

--- sql/jobsourcing.sql
+++ sql/jobsourcing.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Generation Time: Oct 24, 2021 at 04:51 AM
+-- Generation Time: Oct 24, 2021 at 04:58 AM
 -- Server version: 10.4.20-MariaDB
 -- PHP Version: 8.0.9
 
